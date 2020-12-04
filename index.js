2aa6c0aebd79bc9cd0c18f74a1cccad5296ba814
Andrew Simmons <neon.duncan@gmail.com>
2020-12-04 13:05:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyAppendKitten(name) {
   return kittens
 }
 
-function destructivelyPreppendKitten(name) {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens
 }
